6273e12a79ea9e01ac62584bfe11e4a81bc4e997
Ram <rgopinat@redhat.com>
2024-01-27 09:20:46 -0800

    Initial Create

---
+++ src/graphvizInstaller.ts
@@ -0,0 +1,74 @@
+import * as core from "@actions/core";
+import * as io from "@actions/io";
+import { Command } from "./command";
+
+import { FindBinaryStatus } from "./helper";
+
+export class Installer {
+    static async install(runnerOS: string): Promise<FindBinaryStatus> {
+        core.debug(`Runner OS: ${runnerOS}`);
+        switch (runnerOS.toLowerCase()) {
+        case "windows":
+            return Installer.chocoInstall();
+            break;
+        case "linux":
+            return Installer.getAptInstall();
+            break;
+        case "macos":
+            return Installer.brewInstall();
+            break;
+        default:
+            throw new Error(`platform '${runnerOS}' is not yet supported`);
+        }
+    }
+    static async brewInstall(): Promise<FindBinaryStatus> {
+        // run brew update first
+        await Command.execute("brew", [ "update" ]);
+
+        const result = await Command.execute("brew", [ "insstall", "graphviz" ]);
+        if (result.exitCode !== 0) {
+            return {
+                found: false,
+                reason: result.error,
+            };
+        }
+        return {
+            found: true,
+            path: await io.which("dot"),
+        };
+    }
+    static async getAptInstall(): Promise<FindBinaryStatus> {
+        // run apt-get update first
+        await Command.execute("apt-get", [ "update" ]);
+
+        const result = await Command.execute("sudo", [
+            "apt-get", "install", "-y", "graphviz", "libgraphviz-dev", "pkg-config",
+        ]);
+        if (result.exitCode !== 0) {
+            return {
+                found: false,
+                reason: result.error,
+            };
+        }
+        return {
+            found: true,
+            path: await io.which("dot"),
+        };
+    }
+    static async chocoInstall(): Promise<FindBinaryStatus> {
+        const result = await Command.execute("choco", [
+            "install",
+            "graphviz",
+        ]);
+        if (result.exitCode !== 0) {
+            return {
+                found: false,
+                reason: result.error,
+            };
+        }
+        return {
+            found: true,
+            path: await io.which("graphviz"),
+        };
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,73 @@
+import * as core from "@actions/core";
+import * as io from "@actions/io";
+import { Inputs } from "./generated/inputs-outputs";
+import { Command } from "./command";
+import { Installer } from "./graphvizInstaller";
+import { FindBinaryStatus } from "./helper";
+import * as uuidV4 from "uuid/v4";
+import { UploadArtifact } from "./uploadArtifact";
+
+export async function run(): Promise<void> {
+    const runnerOS = process.env.RUNNER_OS || process.platform;
+    const temp = process.env.RUNNER_TEMP;
+
+    const manifestDir = core.getInput(Inputs.MANIFESTS_DIR, { required: true } );
+
+    let roxctl = await io.which("roxctl", false);
+    if (roxctl === "") {
+        core.debug("Couldn't find roxctl in path, defaulting it to current directory");
+        roxctl = "./roxctl";
+    }
+
+    const mapCmdArgs = [
+        "netpol",
+        "connectivity",
+        "map"
+    ];
+    
+    //set manifests directory
+    mapCmdArgs.push(manifestDir);
+
+    //output format
+    mapCmdArgs.push("--output-format=dot");
+
+    //save to file
+    mapCmdArgs.push("--save-to-file=true");
+
+    //set output file
+    mapCmdArgs.push(`--output-file=${temp}/connlist.dot`);
+
+    let result = await Command.execute(roxctl, mapCmdArgs);
+    if (result.exitCode !== 0) {
+        core.setFailed(result.error);
+    }
+
+    let graphViz = await io.which("dot", false);
+    if (graphViz === "") {
+        core.debug("Graphviz not found, installing");
+        const binary: FindBinaryStatus = await Installer.install(runnerOS);
+        if (binary.found === false) {
+            throw new Error(binary.reason);
+        }
+        core.debug("Installed graphviz");
+        graphViz = binary.path;
+    }
+
+    //convert dot file to png
+    const dotCmdArgs = [
+        "-Tpng"
+    ];
+
+    dotCmdArgs.push(`${temp}/connlist.dot > ${temp}/connlist.png`);
+    result = await Command.execute(graphViz, dotCmdArgs);
+    if (result.exitCode !== 0) {
+        core.setFailed(result.error);
+    }
+
+    //upload both dot file and png file as artifacts
+    const artifactName = uuidV4();
+    await UploadArtifact.upload(artifactName, [`${temp}/connlist.dot`, `${temp}/connlist.png`]);
+
+}
+    
+run().catch(core.setFailed);
